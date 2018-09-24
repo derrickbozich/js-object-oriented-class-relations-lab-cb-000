5c073064732b8033997d3dba3d9dc6cac2493311
derrick b <dbozich1@gmail.com>
2018-09-24 02:50:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let store = {drivers: [], passengers: []}
 let driverId = 0;
+let passengerId = 0;
 
 class Driver {
 
@@ -7,5 +8,14 @@ class Driver {
     this.name = name;
     this.id = ++driverId;
     store.drivers.push(this);
+  }
+}
+
+class Passenger {
+
+  constructor(name){
+    this.name = name;
+    this.id = ++passengerId;
+    store.passengers.push(this);
   }
 }
